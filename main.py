cd12cdeed7e70d0adb2d23cc0e21c8772fa6ddac
juststampit <164260695+juststampit@users.noreply.github.com>
2024-03-26 21:55:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -24,34 +24,38 @@ def get_token_data():
 collection_data = get_collection_data()
 token_data = get_token_data()
 
-new_dfs, code = spreadsheet(collection_data)
+# new_dfs, code = spreadsheet(collection_data)
+st.subheader('STAMP Collection Data', divider='red')
 
-st.dataframe(collection_data)
-st.dataframe(token_data)
+st.dataframe(collection_data, use_container_width=True)
 
-code = code if code else "# STAMP Collection Data"
-st.code(code)
+st.subheader('SRC20 Collection Data', divider='red')
 
-def clear_mito_backend_cache():
-    _get_mito_backend.clear()
+st.dataframe(token_data, use_container_width=True)
+
+# code = code if code else "# STAMP Collection Data"
+# st.code(code)
+
+#def clear_mito_backend_cache():
+#    _get_mito_backend.clear()
 
 # Function to cache the last execution time - so we can clear periodically
-@st.cache_resource
-def get_cached_time():
-    # Initialize with a dictionary to store the last execution time
-    return {"last_executed_time": None}
+# @st.cache_resource
+# def get_cached_time():
+#     # Initialize with a dictionary to store the last execution time
+#     return {"last_executed_time": None}
 
-def try_clear_cache():
+# def try_clear_cache():
 
-    # How often to clear the cache
-    CLEAR_DELTA = timedelta(hours=12)
+#     # How often to clear the cache
+#     CLEAR_DELTA = timedelta(hours=12)
 
-    current_time = datetime.now()
-    cached_time = get_cached_time()
+#     current_time = datetime.now()
+#     cached_time = get_cached_time()
 
-    # Check if the current time is different from the cached last execution time
-    if cached_time["last_executed_time"] is None or cached_time["last_executed_time"] + CLEAR_DELTA < current_time:
-        clear_mito_backend_cache()
-        cached_time["last_executed_time"] = current_time
+#     # Check if the current time is different from the cached last execution time
+#     if cached_time["last_executed_time"] is None or cached_time["last_executed_time"] + CLEAR_DELTA < current_time:
+#         clear_mito_backend_cache()
+#         cached_time["last_executed_time"] = current_time
 
-try_clear_cache()
+# try_clear_cache()
